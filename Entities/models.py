80369a99722d100b0ea545dcd52dfdaf2e596476
GuyHan623 <guyhness@gmail.com>
2024-10-08 12:44:06 +0300

    Finish createing entities classes, added Agent and Dispatch

--- Entities/models.py
+++ Entities/models.py
@@ -15,3 +15,20 @@ class Message(models.Model):
 
     def __str__(self):
         return self.id
+    
+class Agent(models.Model):
+    id = models.AutoField(primary_key=True)
+    divisionid = models.ForeignKey(Division, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.id
+    
+
+class Dispatch(models.Model):
+    id = models.AutoField(primary_key=True)
+    messageid = models.ForeignKey(Message, on_delete=models.CASCADE)
+    agentid = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    
+    def __str__(self):
+        return self.id
